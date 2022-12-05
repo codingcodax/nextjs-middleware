537ec8a7ede3e5ff037b3dd5dd986fc1b98cfbfc
Alexis Guzman <codingcodax@gmail.com>
2022-12-04 21:07:50 -0600

    feat(api): make get greeting endpoint dynamic

---
+++ pages/api/get-greeting/[slug].ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  greeting?: string;
+  error?: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const name = req.query["name"];
+
+  if (!name || typeof name !== "string") {
+    res.status(404).json({ error: "Please use with a valid name" });
+
+    return;
+  }
+
+  if (name !== "john") {
+    res.status(404).json({ error: "Invalid name" });
+
+    return;
+  }
+
+  res.status(200).json({ greeting: `Hello ${name} doe!` });
+}
